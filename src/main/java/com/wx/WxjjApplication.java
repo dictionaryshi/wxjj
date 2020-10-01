86c1970a1d455bb6a836037cf54fbf32a78da807
史春阳 <shichunyang02@meituan.com>
2020-10-01 17:33:32 +0800

    WxjjApplication.java

--- src/main/java/com/wx/WxjjApplication.java
+++ src/main/java/com/wx/WxjjApplication.java
@@ -1,8 +1,10 @@
 package com.wx;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -11,7 +13,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @author shichunyang
  */
 @EnableTransactionManagement
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        MybatisAutoConfiguration.class,
+        KafkaAutoConfiguration.class,
+})
 public class WxjjApplication {
 
     public static void main(String[] args) {
