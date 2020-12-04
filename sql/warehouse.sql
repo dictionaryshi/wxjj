a5026b40c8392d6f3c3a3b2535e8995a83dc6e05
史春阳 <shichunyang02@meituan.com>
2020-12-04 20:31:05 +0800

    warehouse.sql

--- sql/warehouse.sql
+++ sql/warehouse.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user_passport` (
   `passport` varchar(50) NOT NULL DEFAULT '' COMMENT '账号',
   `password` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uniq_passport` (`passport`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户账号';
