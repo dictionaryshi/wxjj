aed300cd6b084b1da47d71fe8af913e7922f1a85
史春阳 <shichunyang02@meituan.com>
2020-12-07 16:33:36 +0800

    db config redis config

---
+++ src/main/java/com/wx/dao/warehouse/model/UserPassportDO.java
@@ -0,0 +1,55 @@
+package com.wx.dao.warehouse.model;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserPassportDO {
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String passport;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 创建时间
+     */
+    private Date createdAt;
+
+    /**
+     * 修改时间
+     */
+    private Date updatedAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport == null ? null : passport.trim();
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
