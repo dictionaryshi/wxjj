5fba2b320ddd2d5764804b7f0cfa742aca1e4d3f
史春阳 <shichunyang02@meituan.com>
2020-12-22 22:13:35 +0800

    UserPassportResponse.java

--- src/main/java/com/wx/controller/response/UserPassportResponse.java
+++ src/main/java/com/wx/controller/response/UserPassportResponse.java
@@ -1,5 +1,8 @@
 package com.wx.controller.response;
 
+import com.scy.core.format.DateUtil;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +16,7 @@ import java.util.Date;
  * ---------------------------------------
  * Desc    : 用户账号
  */
+@ApiModel(value = "用户账号信息")
 @Getter
 @Setter
 @ToString
@@ -21,15 +25,18 @@ public class UserPassportResponse {
     /**
      * 用户id
      */
+    @ApiModelProperty(value = "用户唯一标识", required = true, example = "13264232894")
     private Long userId;
 
     /**
      * 账号
      */
+    @ApiModelProperty(value = "用户账号", required = true)
     private String passport;
 
     /**
      * 创建时间
      */
+    @ApiModelProperty(value = "用户创建时间", required = true, example = DateUtil.DEFAULT_TIME)
     private Date createdAt;
 }
