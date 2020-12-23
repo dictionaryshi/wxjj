0cb37230fbe9ba8435359257f962cf4c7a4e577d
史春阳 <shichunyang02@meituan.com>
2020-12-23 15:08:48 +0800

    warehouse.sql

--- sql/warehouse.sql
+++ sql/warehouse.sql
@@ -8,3 +8,12 @@ CREATE TABLE `user_passport` (
   UNIQUE KEY `uniq_passport` (`passport`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户账号';
 
+CREATE TABLE `sku_category` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '品类id',
+  `category_name` varchar(256) NOT NULL DEFAULT '' COMMENT '品类名称',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_category_name` (`category_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品分类';
+
