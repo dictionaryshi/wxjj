09e1dd3e69cc9c4d4a57f0009b5a09a2867cddf5
史春阳 <shichunyang02@meituan.com>
2020-12-24 10:04:05 +0800

    SkuCategoryDomainService.java

---
+++ src/main/java/com/wx/domain/category/service/SkuCategoryDomainService.java
@@ -0,0 +1,16 @@
+package com.wx.domain.category.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : shichunyang
+ * Date    : 2020/12/24
+ * Time    : 10:00 上午
+ * ---------------------------------------
+ * Desc    : 商品分类领域服务
+ */
+@Slf4j
+@Service
+public class SkuCategoryDomainService {
+}
