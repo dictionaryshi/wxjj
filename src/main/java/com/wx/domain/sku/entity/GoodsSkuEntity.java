4dd46912f4005e5ee82e25b8ca2bcba23ea3cd37
史春阳 <shichunyang02@meituan.com>
2020-12-25 10:39:10 +0800

    GoodsSkuEntity.java

---
+++ src/main/java/com/wx/domain/sku/entity/GoodsSkuEntity.java
@@ -0,0 +1,33 @@
+package com.wx.domain.sku.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author : shichunyang
+ * Date    : 2020/12/25
+ * Time    : 10:36 上午
+ * ---------------------------------------
+ * Desc    : 商品实体
+ */
+@Getter
+@Setter
+@ToString
+public class GoodsSkuEntity {
+
+    /**
+     * 商品id
+     */
+    private Long skuId;
+
+    /**
+     * 商品名称
+     */
+    private String skuName;
+
+    /**
+     * 品类id
+     */
+    private Long categoryId;
+}
