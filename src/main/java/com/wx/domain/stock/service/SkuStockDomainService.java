dbdab5de12e6df30dd8e206a5a5365bd92968f2f
史春阳 <shichunyang02@meituan.com>
2021-01-29 22:28:11 +0800

    SkuStockDomainService.java

---
+++ src/main/java/com/wx/domain/stock/service/SkuStockDomainService.java
@@ -0,0 +1,94 @@
+package com.wx.domain.stock.service;
+
+import com.scy.core.CollectionUtil;
+import com.scy.core.exception.BusinessException;
+import com.scy.core.format.MessageUtil;
+import com.scy.db.util.ForceMasterHelper;
+import com.scy.redis.lock.RedisLock;
+import com.wx.dao.warehouse.mapper.extend.SkuStockDOMapperExtend;
+import com.wx.dao.warehouse.model.SkuStockDO;
+import com.wx.dao.warehouse.model.SkuStockDOExample;
+import com.wx.domain.stock.entity.SkuStockEntity;
+import com.wx.domain.stock.entity.StockOperateValueobject;
+import com.wx.domain.stock.factory.SkuStockFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author : shichunyang
+ * Date    : 2021/1/29
+ * Time    : 8:21 下午
+ * ---------------------------------------
+ * Desc    : 商品库存领域服务
+ */
+@Slf4j
+@Service
+public class SkuStockDomainService {
+
+    @Autowired
+    private SkuStockDOMapperExtend skuStockDOMapper;
+
+    @Autowired
+    private RedisLock redisLock;
+
+    public long insert(SkuStockEntity skuStockEntity) {
+        Optional<SkuStockDO> skuStockOptional = SkuStockFactory.toSkuStockDO(skuStockEntity);
+        if (!skuStockOptional.isPresent()) {
+            throw new BusinessException("库存实体不存在");
+        }
+
+        SkuStockDO skuStockDO = skuStockOptional.get();
+        skuStockDOMapper.insertSelective(skuStockDO);
+        return skuStockDO.getId();
+    }
+
+    public Optional<SkuStockEntity> getSkuStockEntity(long stockBaseInfoId, long skuId) {
+        SkuStockDOExample skuStockDOExample = new SkuStockDOExample();
+        SkuStockDOExample.Criteria criteria = skuStockDOExample.createCriteria();
+        criteria.andStockBaseInfoIdEqualTo(stockBaseInfoId);
+        criteria.andSkuIdEqualTo(skuId);
+        List<SkuStockDO> skuStocks = skuStockDOMapper.selectByExample(skuStockDOExample);
+        SkuStockDO skuStockDO = CollectionUtil.firstElement(skuStocks);
+        return SkuStockFactory.toSkuStockEntity(skuStockDO);
+    }
+
+    public Optional<Long> getSkuStock(long stockBaseInfoId, long skuId) {
+        Optional<SkuStockEntity> skuStockEntityOptional = getSkuStockEntity(stockBaseInfoId, skuId);
+        return skuStockEntityOptional.map(SkuStockEntity::getStock);
+    }
+
+    public StockOperateValueobject update(SkuStockEntity skuStockEntity) {
+        String lockKey = skuStockEntity.getLockKey();
+        try {
+            redisLock.lock(lockKey);
+            Optional<SkuStockEntity> skuStockEntityOptional = getSkuStockEntity(skuStockEntity.getStockBaseInfoId(), skuStockEntity.getSkuId());
+            if (!skuStockEntityOptional.isPresent()) {
+                throw new BusinessException(MessageUtil.format("库存不存在",
+                        "stockBaseInfoId", skuStockEntity.getStockBaseInfoId(), "skuId", skuStockEntity.getSkuId()));
+            }
+
+            SkuStockEntity skuStockEntityForUpdate = new SkuStockEntity();
+            skuStockEntityForUpdate.setId(skuStockEntityOptional.get().getId());
+            skuStockEntityForUpdate.setStock(skuStockEntity.getStock());
+            skuStockEntityForUpdate.operateStock(skuStockEntity.getStock());
+            Optional<SkuStockDO> skuStockOptional = SkuStockFactory.toSkuStockDO(skuStockEntityForUpdate);
+            skuStockOptional.ifPresent(skuStockDO -> skuStockDOMapper.updateByPrimaryKeySelective(skuStockDO));
+
+            try {
+                ForceMasterHelper.forceMaster();
+                skuStockEntityForUpdate.operateStockAfter(skuStockEntityOptional.get().getStock(),
+                        getSkuStock(skuStockEntity.getStockBaseInfoId(), skuStockEntity.getSkuId()).orElse(null));
+            } finally {
+                ForceMasterHelper.clearForceMaster();
+            }
+
+            return skuStockEntityForUpdate.getStockOperateValueobject();
+        } finally {
+            redisLock.unlock(lockKey);
+        }
+    }
+}
