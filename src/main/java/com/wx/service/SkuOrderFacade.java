975915c6e8df6d11ae0db7825ae7b96dfb754fc2
史春阳 <shichunyang02@meituan.com>
2021-02-02 16:57:37 +0800

    SkuOrderFacade.java

--- src/main/java/com/wx/service/SkuOrderFacade.java
+++ src/main/java/com/wx/service/SkuOrderFacade.java
@@ -2,8 +2,12 @@ package com.wx.service;
 
 import com.wx.domain.order.entity.SkuOrderEntity;
 import com.wx.domain.order.service.SkuOrderDomainService;
+import com.wx.domain.passport.service.UserPassportDomainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author : shichunyang
@@ -18,10 +22,25 @@ public class SkuOrderFacade {
     @Autowired
     private SkuOrderDomainService skuOrderDomainService;
 
+    @Autowired
+    private UserPassportDomainService userPassportDomainService;
+
     /**
      * 创建出库/入库订单
      */
     public long insertSkuOrder(SkuOrderEntity skuOrderEntity) {
         return skuOrderDomainService.insertSkuOrder(skuOrderEntity);
+    }
+
+    /**
+     * 查询订单
+     */
+    public Optional<SkuOrderEntity> getOrder(long orderId) {
+        Optional<SkuOrderEntity> skuOrderEntityOptional = skuOrderDomainService.getOrder(orderId);
+        skuOrderEntityOptional.ifPresent(skuOrderEntity -> {
+            Map<Long, String> allPassportMap = userPassportDomainService.getAllPassportMap();
+            skuOrderEntity.setOperatorName(allPassportMap.get(skuOrderEntity.getOperator()));
+        });
+        return skuOrderEntityOptional;
     }
 }
