221389f564253df560de07a5dd8411cc346e9298
史春阳 <shichunyang02@meituan.com>
2021-02-04 14:04:50 +0800

    SkuOrderFacade.java

---
+++ src/main/java/com/wx/constant/CommonConstant.java
@@ -0,0 +1,17 @@
+package com.wx.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : shichunyang
+ * Date    : 2021/2/4
+ * Time    : 1:16 下午
+ * ---------------------------------------
+ * Desc    : 公共常量
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommonConstant {
+
+    public static final String WAREHOUSE = "warehouse";
+}
