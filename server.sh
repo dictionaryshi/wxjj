cd46e7a34da650c8503c3b8afa6a31b0af599de5
史春阳 <shichunyang02@meituan.com>
2021-02-05 23:59:25 +0800

    server.sh

--- server.sh
+++ server.sh
@@ -8,11 +8,26 @@ server_start="-server -Xms256m -Xmx256m -Xmn128m -Xss256k -XX:MetaspaceSize=256m
 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC ${server_name}"
 
 function monitor() {
+  processId=$(getProcessId)
+  echo "${server_name} 进程id=> ${processId}"
+  if [[ ${processId} ]]; then
+    echo "test url=> ${server_test_url}"
+  else
+    echo -e "\e[1;31m ${server_name} 进程id不存在, 开始启动服务... \e[0m"
+    nohup java -jar ${server_start} > ${server_log_file} 2>&1 &
+    echo -e "\e[1;31m ${server_name} 启动完毕 \e[0m"
+    sleep 60
+  fi
+}
+
+function getProcessId() {
+    processId=$(jps -l | grep ${server_name} | awk '{print $1}')
+    echo ${processId}
 }
 
 while true
 do
   monitor>>${log_file}
-  sleep 2
+  sleep 10
 done
 
