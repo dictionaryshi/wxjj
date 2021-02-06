57c7df4ff6d33908d97b51708f4863c3ffea75bd
史春阳 <shichunyang02@meituan.com>
2021-02-06 10:45:20 +0800

    server.sh

--- server.sh
+++ server.sh
@@ -12,6 +12,12 @@ function monitor() {
   echo "${server_name} 进程id=> ${processId}"
   if [[ ${processId} ]]; then
     echo "test url=> ${server_test_url}"
+    http_code=$(curl -s -o /dev/null -m 5 --connect-timeout 5 ${server_test_url} -w "%{http_code}")
+    if [ ${http_code} -eq 200 ]; then
+      echo -e "\e[1;32m ${server_name} 服务正常 \e[0m"
+    else
+      echo -e "\e[1;31m ${server_name} 宕机, 开始重启... \e[0m"
+    fi
   else
     echo -e "\e[1;31m ${server_name} 进程id不存在, 开始启动服务... \e[0m"
     nohup java -jar ${server_start} > ${server_log_file} 2>&1 &
