a28ac8e667d31acf8124de50b55c20a35bfe7336
史春阳 <shichunyang02@meituan.com>
2022-01-06 14:55:28 +0800

    ClientHandler.java

--- src/main/java/com/wx/netty/client/ClientHandler.java
+++ src/main/java/com/wx/netty/client/ClientHandler.java
@@ -1,9 +1,7 @@
 package com.wx.netty.client;
 
-import com.wx.netty.protocol.LoginRequestPacket;
-import com.wx.netty.protocol.LoginResponsePacket;
-import com.wx.netty.protocol.Packet;
-import com.wx.netty.protocol.PacketCodeC;
+import com.wx.netty.attribute.LoginUtil;
+import com.wx.netty.protocol.*;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -49,9 +47,13 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
 
             if (loginResponsePacket.isSuccess()) {
                 System.out.println(new Date() + ": 客户端登录成功");
+                LoginUtil.markAsLogin(ctx.channel());
             } else {
                 System.out.println(new Date() + ": 客户端登录失败，原因：" + loginResponsePacket.getReason());
             }
+        } else if (packet instanceof MessageResponsePacket) {
+            MessageResponsePacket messageResponsePacket = (MessageResponsePacket) packet;
+            System.out.println(new Date() + ": 收到服务端的消息: " + messageResponsePacket.getMessage());
         }
     }
 }
