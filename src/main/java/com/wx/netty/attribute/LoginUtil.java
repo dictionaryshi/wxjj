b8dd877a768656617b5d6d027dff97941029d37c
史春阳 <shichunyang02@meituan.com>
2022-01-06 13:58:03 +0800

    LoginUtil.java

---
+++ src/main/java/com/wx/netty/attribute/LoginUtil.java
@@ -0,0 +1,17 @@
+package com.wx.netty.attribute;
+
+import io.netty.channel.Channel;
+import io.netty.util.Attribute;
+
+public class LoginUtil {
+
+    public static void markAsLogin(Channel channel) {
+        channel.attr(Attributes.LOGIN).set(true);
+    }
+
+    public static boolean hasLogin(Channel channel) {
+        Attribute<Boolean> loginAttr = channel.attr(Attributes.LOGIN);
+
+        return loginAttr.get() != null;
+    }
+}
