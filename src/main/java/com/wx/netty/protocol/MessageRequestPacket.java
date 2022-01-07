44ad92a7f3944368ea6cf966eee7ea200007960c
史春阳 <shichunyang02@meituan.com>
2022-01-07 09:27:56 +0800

    MessageRequestPacket.java

--- src/main/java/com/wx/netty/protocol/MessageRequestPacket.java
+++ src/main/java/com/wx/netty/protocol/MessageRequestPacket.java
@@ -9,6 +9,13 @@ public class MessageRequestPacket extends Packet {
 
     private String message;
 
+    public MessageRequestPacket() {
+    }
+
+    public MessageRequestPacket(String message) {
+        this.message = message;
+    }
+
     @Override
     public int getCommand() {
         return MESSAGE_REQUEST;
