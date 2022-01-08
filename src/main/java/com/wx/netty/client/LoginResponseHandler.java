cbc42f5f5383dec946fe55959d899d8632c135d9
史春阳 <shichunyang02@meituan.com>
2022-01-08 22:07:32 +0800

    LoginResponseHandler.java

--- src/main/java/com/wx/netty/client/LoginResponseHandler.java
+++ src/main/java/com/wx/netty/client/LoginResponseHandler.java
@@ -1,15 +1,10 @@
 package com.wx.netty.client;
 
-import com.wx.netty.attribute.LoginUtil;
-import com.wx.netty.protocol.LoginRequestPacket;
+import com.wx.netty.attribute.SessionUtil;
 import com.wx.netty.protocol.LoginResponsePacket;
-import com.wx.netty.protocol.PacketCodeC;
-import io.netty.buffer.ByteBuf;
+import com.wx.netty.session.Session;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-
-import java.util.Date;
-import java.util.UUID;
 
 /**
  * @author : shichunyang
@@ -20,34 +15,23 @@ import java.util.UUID;
  */
 public class LoginResponseHandler extends SimpleChannelInboundHandler<LoginResponsePacket> {
 
-    /**
-     * 这个方法会在客户端连接建立成功之后被调用
-     */
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        // 创建登录对象
-        LoginRequestPacket loginRequestPacket = new LoginRequestPacket();
-        loginRequestPacket.setUserId(UUID.randomUUID().toString());
-        loginRequestPacket.setUsername("shichunyang");
-        loginRequestPacket.setPassword("naodian12300");
-
-        // 写数据
-        ctx.channel().writeAndFlush(loginRequestPacket);
-    }
-
     @Override
     public void channelRead0(ChannelHandlerContext ctx, LoginResponsePacket loginResponsePacket) throws Exception {
+        String userId = loginResponsePacket.getUserId();
+        String userName = loginResponsePacket.getUserName();
+
         if (loginResponsePacket.isSuccess()) {
-            System.out.println(new Date() + ": 客户端登录成功");
-            LoginUtil.markAsLogin(ctx.channel());
+            System.out.println("[" + userName + "]登录成功，userId 为: " + loginResponsePacket.getUserId());
+
+            SessionUtil.bindSession(new Session(userId, userName), ctx.channel());
         } else {
-            System.out.println(new Date() + ": 客户端登录失败，原因：" + loginResponsePacket.getReason());
+            System.out.println("[" + userName + "]登录失败，原因：" + loginResponsePacket.getReason());
         }
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("客户端连接被关闭!");
+        System.out.println("客户端连接关闭!");
         super.channelInactive(ctx);
     }
 }
