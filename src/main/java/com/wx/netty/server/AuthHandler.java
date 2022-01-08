7435afa7b9effe6bddfd88f8934bf0d4803c4a87
史春阳 <shichunyang02@meituan.com>
2022-01-08 22:15:42 +0800

    AuthHandler.java

--- src/main/java/com/wx/netty/server/AuthHandler.java
+++ src/main/java/com/wx/netty/server/AuthHandler.java
@@ -1,6 +1,6 @@
 package com.wx.netty.server;
 
-import com.wx.netty.attribute.LoginUtil;
+import com.wx.netty.attribute.SessionUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -15,20 +15,11 @@ public class AuthHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (!LoginUtil.hasLogin(ctx.channel())) {
+        if (!SessionUtil.hasLogin(ctx.channel())) {
             ctx.channel().close();
         } else {
             ctx.pipeline().remove(this);
             super.channelRead(ctx, msg);
-        }
-    }
-
-    @Override
-    public void handlerRemoved(ChannelHandlerContext ctx) {
-        if (LoginUtil.hasLogin(ctx.channel())) {
-            System.out.println("当前连接登录验证完毕，无需再次验证, AuthHandler 被移除");
-        } else {
-            System.out.println("无登录验证，强制关闭连接!");
         }
     }
 }
