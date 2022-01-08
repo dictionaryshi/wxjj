b31a3aca96ccec43515e9ec0f1ad6beab9eacfbc
史春阳 <shichunyang02@meituan.com>
2022-01-08 21:25:35 +0800

    SessionUtil.java

---
+++ src/main/java/com/wx/netty/attribute/SessionUtil.java
@@ -0,0 +1,38 @@
+package com.wx.netty.attribute;
+
+import com.wx.netty.session.Session;
+import io.netty.channel.Channel;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionUtil {
+
+    private static final Map<String, Channel> USER_ID_CHANNEL_MAP = new ConcurrentHashMap<>();
+
+    public static void bindSession(Session session, Channel channel) {
+        USER_ID_CHANNEL_MAP.put(session.getUserId(), channel);
+
+        channel.attr(Attributes.SESSION).set(session);
+    }
+
+    public static void unBindSession(Channel channel) {
+        if (hasLogin(channel)) {
+            USER_ID_CHANNEL_MAP.remove(getSession(channel).getUserId());
+
+            channel.attr(Attributes.SESSION).set(null);
+        }
+    }
+
+    public static boolean hasLogin(Channel channel) {
+        return channel.hasAttr(Attributes.SESSION);
+    }
+
+    public static Session getSession(Channel channel) {
+        return channel.attr(Attributes.SESSION).get();
+    }
+
+    public static Channel getChannel(String userId) {
+        return USER_ID_CHANNEL_MAP.get(userId);
+    }
+}
