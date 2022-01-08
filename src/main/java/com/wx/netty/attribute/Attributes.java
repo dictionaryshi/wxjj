4737651418a156690616b54a63ee5aed09046045
史春阳 <shichunyang02@meituan.com>
2022-01-08 21:12:09 +0800

    Attributes.java

--- src/main/java/com/wx/netty/attribute/Attributes.java
+++ src/main/java/com/wx/netty/attribute/Attributes.java
@@ -1,7 +1,9 @@
 package com.wx.netty.attribute;
 
+import com.wx.netty.session.Session;
 import io.netty.util.AttributeKey;
 
 public interface Attributes {
-    AttributeKey<Boolean> LOGIN = AttributeKey.newInstance("login");
+
+    AttributeKey<Session> SESSION = AttributeKey.newInstance("session");
 }
