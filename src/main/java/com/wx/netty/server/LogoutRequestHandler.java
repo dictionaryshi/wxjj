2df2b71a8df33ca07d52fb1e4a41642a19bb1d71
史春阳 <shichunyang02@meituan.com>
2022-01-09 12:43:19 +0800

    群聊handler

---
+++ src/main/java/com/wx/netty/server/LogoutRequestHandler.java
@@ -0,0 +1,23 @@
+package com.wx.netty.server;
+
+import com.wx.netty.attribute.SessionUtil;
+import com.wx.netty.protocol.LogoutRequestPacket;
+import com.wx.netty.protocol.LogoutResponsePacket;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * 登出请求
+ */
+public class LogoutRequestHandler extends SimpleChannelInboundHandler<LogoutRequestPacket> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, LogoutRequestPacket msg) {
+        SessionUtil.unBindSession(ctx.channel());
+
+        LogoutResponsePacket logoutResponsePacket = new LogoutResponsePacket();
+        logoutResponsePacket.setSuccess(true);
+
+        ctx.channel().writeAndFlush(logoutResponsePacket);
+    }
+}
