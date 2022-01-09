783c41cc9fe2c166a2d575c17ae719128f4fd50b
史春阳 <shichunyang02@meituan.com>
2022-01-09 17:23:58 +0800

    ListGroupMembersResponseHandler.java

---
+++ src/main/java/com/wx/netty/client/ListGroupMembersResponseHandler.java
@@ -0,0 +1,12 @@
+package com.wx.netty.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class ListGroupMembersResponseHandler extends SimpleChannelInboundHandler<ListGroupMembersResponsePacket> {
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ListGroupMembersResponsePacket responsePacket) {
+        System.out.println("群[" + responsePacket.getGroupId() + "]中的人包括：" + responsePacket.getSessionList());
+    }
+}
