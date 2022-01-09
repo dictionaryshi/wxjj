1209bdae41aec14e286a0fb1e5415ea126c41139
史春阳 <shichunyang02@meituan.com>
2022-01-09 18:38:36 +0800

    NettyServer.java

--- src/main/java/com/wx/netty/NettyServer.java
+++ src/main/java/com/wx/netty/NettyServer.java
@@ -55,10 +55,20 @@ public class NettyServer {
                     public void initChannel(NioSocketChannel nioSocketChannel) {
                         nioSocketChannel.pipeline().addLast(new Spliter());
                         nioSocketChannel.pipeline().addLast(new PacketDecoder());
+                        // 登录请求处理器
                         nioSocketChannel.pipeline().addLast(new LoginRequestHandler());
                         nioSocketChannel.pipeline().addLast(new AuthHandler());
+                        // 单聊消息请求处理器
                         nioSocketChannel.pipeline().addLast(new MessageRequestHandler());
+                        // 创建群请求处理器
                         nioSocketChannel.pipeline().addLast(new CreateGroupRequestHandler());
+                        // 加群请求处理器
+                        nioSocketChannel.pipeline().addLast(new JoinGroupRequestHandler());
+                        // 退群请求处理器
+                        nioSocketChannel.pipeline().addLast(new QuitGroupRequestHandler());
+                        // 获取群成员请求处理器
+                        nioSocketChannel.pipeline().addLast(new ListGroupMembersRequestHandler());
+                        // 登出请求处理器
                         nioSocketChannel.pipeline().addLast(new LogoutRequestHandler());
                         nioSocketChannel.pipeline().addLast(new PacketEncoder());
                     }
