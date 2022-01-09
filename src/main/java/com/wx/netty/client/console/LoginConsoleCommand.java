7085f04df5ba5c8c692787b3834ad2d8952a36e2
史春阳 <shichunyang02@meituan.com>
2022-01-09 10:40:07 +0800

    console

---
+++ src/main/java/com/wx/netty/client/console/LoginConsoleCommand.java
@@ -0,0 +1,30 @@
+package com.wx.netty.client.console;
+
+import com.wx.netty.protocol.LoginRequestPacket;
+import io.netty.channel.Channel;
+
+import java.util.Scanner;
+
+public class LoginConsoleCommand implements ConsoleCommand {
+
+    @Override
+    public void exec(Scanner scanner, Channel channel) {
+        System.out.print("输入用户名登录: ");
+
+        LoginRequestPacket loginRequestPacket = new LoginRequestPacket();
+        loginRequestPacket.setUserName(scanner.nextLine());
+        loginRequestPacket.setPassword("pwd");
+
+        // 发送登录数据包
+        channel.writeAndFlush(loginRequestPacket);
+
+        waitForLoginResponse();
+    }
+
+    private static void waitForLoginResponse() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
