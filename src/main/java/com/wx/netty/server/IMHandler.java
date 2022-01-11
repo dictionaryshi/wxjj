ad25d39ff11efaf16be271580fe16d721b86b20a
史春阳 <shichunyang02@meituan.com>
2022-01-11 10:10:58 +0800

    IMHandler.java

---
+++ src/main/java/com/wx/netty/server/IMHandler.java
@@ -0,0 +1,36 @@
+package com.wx.netty.server;
+
+import com.wx.netty.protocol.Packet;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.wx.netty.protocol.Command.*;
+
+@ChannelHandler.Sharable
+public class IMHandler extends SimpleChannelInboundHandler<Packet> {
+
+    public static final IMHandler INSTANCE = new IMHandler();
+
+    private final Map<Integer, SimpleChannelInboundHandler<? extends Packet>> handlerMap;
+
+    private IMHandler() {
+        handlerMap = new HashMap<>();
+
+        handlerMap.put(MESSAGE_REQUEST, MessageRequestHandler.INSTANCE);
+        handlerMap.put(LOGOUT_REQUEST, LogoutRequestHandler.INSTANCE);
+        handlerMap.put(CREATE_GROUP_REQUEST, CreateGroupRequestHandler.INSTANCE);
+        handlerMap.put(LIST_GROUP_MEMBERS_REQUEST, ListGroupMembersRequestHandler.INSTANCE);
+        handlerMap.put(JOIN_GROUP_REQUEST, JoinGroupRequestHandler.INSTANCE);
+        handlerMap.put(QUIT_GROUP_REQUEST, QuitGroupRequestHandler.INSTANCE);
+        handlerMap.put(GROUP_MESSAGE_REQUEST, GroupMessageRequestHandler.INSTANCE);
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Packet packet) throws Exception {
+        handlerMap.get(packet.getCommand()).channelRead(ctx, packet);
+    }
+}
