87091a807c255b1659345a9ef6c3c270c5533371
史春阳 <shichunyang02@meituan.com>
2022-01-13 12:23:11 +0800

    MessageRequestHandler.java

--- src/main/java/com/wx/netty/server/MessageRequestHandler.java
+++ src/main/java/com/wx/netty/server/MessageRequestHandler.java
@@ -1,13 +1,17 @@
 package com.wx.netty.server;
 
+import com.scy.core.StringUtil;
 import com.wx.netty.attribute.SessionUtil;
 import com.wx.netty.protocol.MessageRequestPacket;
 import com.wx.netty.protocol.MessageResponsePacket;
 import com.wx.netty.session.Session;
+import com.wx.netty.util.NettyUtil;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.Objects;
 
 /**
  * @author : shichunyang
@@ -26,14 +30,12 @@ public class MessageRequestHandler extends SimpleChannelInboundHandler<MessageRe
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, MessageRequestPacket messageRequestPacket) throws Exception {
-        long begin = System.currentTimeMillis();
-
         // 消息发送方的session
         Session session = SessionUtil.getSession(ctx.channel());
 
         MessageResponsePacket messageResponsePacket = new MessageResponsePacket();
-        messageResponsePacket.setFromUserId(session.getUserId());
-        messageResponsePacket.setFromUserName(session.getUserName());
+        messageResponsePacket.setFromUserId(Objects.isNull(session) ? StringUtil.EMPTY : session.getUserId());
+        messageResponsePacket.setFromUserName(Objects.isNull(session) ? StringUtil.EMPTY : session.getUserName());
         messageResponsePacket.setMessage(messageRequestPacket.getMessage());
 
         // 消息接收方的 channel
@@ -41,11 +43,7 @@ public class MessageRequestHandler extends SimpleChannelInboundHandler<MessageRe
 
         // 将消息发送给消息接收方
         if (toUserChannel != null && SessionUtil.hasLogin(toUserChannel)) {
-            toUserChannel.writeAndFlush(messageResponsePacket).addListener(future -> {
-                if (future.isDone()) {
-
-                }
-            });
+            NettyUtil.sendMsg(ctx.channel(), toUserChannel, messageResponsePacket);
         } else {
             System.err.println("[" + messageRequestPacket.getToUserId() + "] 不在线，发送失败!");
         }
