3a0d3e86b0d9360255b1effc46d27ca6971dfcec
史春阳 <shichunyang02@meituan.com>
2022-01-15 19:43:59 +0800

    IMIdleStateHandler.java

--- src/main/java/com/wx/netty/handler/IMIdleStateHandler.java
+++ src/main/java/com/wx/netty/handler/IMIdleStateHandler.java
@@ -1,11 +1,18 @@
 package com.wx.netty.handler;
 
+import com.scy.core.ObjectUtil;
+import com.scy.core.format.DateUtil;
+import com.scy.core.format.MessageUtil;
+import com.wx.netty.attribute.Attributes;
+import com.wx.netty.util.NettyUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class IMIdleStateHandler extends IdleStateHandler {
 
     private static final int READER_IDLE_TIME = 15;
@@ -16,7 +23,10 @@ public class IMIdleStateHandler extends IdleStateHandler {
 
     @Override
     public void channelIdle(ChannelHandlerContext ctx, IdleStateEvent evt) throws Exception {
-        System.out.println(READER_IDLE_TIME + "秒内未读到数据，关闭连接");
+        long lastReadTime = ObjectUtil.obj2Long(NettyUtil.getAttr(ctx.channel(), Attributes.LAST_READ_TIME), 0L);
+        long spacing = System.currentTimeMillis() - lastReadTime;
+        log.info(MessageUtil.format("心跳channelIdle", "spacing", DateUtil.millisecond2Second(spacing)));
+
         ctx.channel().close();
     }
 }
