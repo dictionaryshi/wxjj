e333618e49830dc698939f9776543ee73b67c599
史春阳 <shichunyang02@meituan.com>
2022-02-14 10:58:28 +0800

    Command.java

--- src/main/java/com/wx/netty/protocol/Command.java
+++ src/main/java/com/wx/netty/protocol/Command.java
@@ -14,26 +14,6 @@ public interface Command {
 
     int LOGOUT_RESPONSE = 6;
 
-    int CREATE_GROUP_REQUEST = 7;
-
-    int CREATE_GROUP_RESPONSE = 8;
-
-    int LIST_GROUP_MEMBERS_REQUEST = 9;
-
-    int LIST_GROUP_MEMBERS_RESPONSE = 10;
-
-    int JOIN_GROUP_REQUEST = 11;
-
-    int JOIN_GROUP_RESPONSE = 12;
-
-    int QUIT_GROUP_REQUEST = 13;
-
-    int QUIT_GROUP_RESPONSE = 14;
-
-    int GROUP_MESSAGE_REQUEST = 15;
-
-    int GROUP_MESSAGE_RESPONSE = 16;
-
     int HEARTBEAT_REQUEST = 17;
 
     int HEARTBEAT_RESPONSE = 18;
