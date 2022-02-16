5b691688413403e50db5e683a9a9ec11f7ec8265
史春阳 <shichunyang02@meituan.com>
2022-02-16 19:54:46 +0800

    NettyClient.java

--- src/main/java/com/wx/netty/NettyClient.java
+++ src/main/java/com/wx/netty/NettyClient.java
@@ -4,11 +4,9 @@ import com.scy.netty.client.ClientConfig;
 import com.scy.netty.util.SessionUtil;
 import com.wx.netty.client.console.ConsoleCommandManager;
 import com.wx.netty.client.console.LoginConsoleCommand;
-import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author : shichunyang
@@ -21,29 +19,23 @@ import java.util.concurrent.TimeUnit;
 public class NettyClient {
 
     public static void main(String[] args) throws Exception {
-        ClientConfig clientConfig = new ClientConfig();
-
         com.scy.netty.client.NettyClient nettyClient = new com.scy.netty.client.NettyClient();
-        nettyClient.init("127.0.0.1:8000", clientConfig);
+        nettyClient.init("127.0.0.1:8000", new ClientConfig());
 
-        startConsoleThread(nettyClient.getChannel());
-
-        TimeUnit.SECONDS.sleep(30);
-
-        clientConfig.stop();
+        startConsoleThread(nettyClient);
     }
 
-    private static void startConsoleThread(Channel channel) {
+    private static void startConsoleThread(com.scy.netty.client.NettyClient nettyClient) {
         ConsoleCommandManager consoleCommandManager = new ConsoleCommandManager();
         LoginConsoleCommand loginConsoleCommand = new LoginConsoleCommand();
         Scanner scanner = new Scanner(System.in);
 
         new Thread(() -> {
-            while (channel.isActive()) {
-                if (!SessionUtil.hasLogin(channel)) {
-                    loginConsoleCommand.exec(scanner, channel);
+            while (nettyClient.getChannel().isActive()) {
+                if (!SessionUtil.hasLogin(nettyClient.getChannel())) {
+                    loginConsoleCommand.exec(scanner, nettyClient.getChannel());
                 } else {
-                    consoleCommandManager.exec(scanner, channel);
+                    consoleCommandManager.exec(scanner, nettyClient.getChannel());
                 }
             }
         }).start();
