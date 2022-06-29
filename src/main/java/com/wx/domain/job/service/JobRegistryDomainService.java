2121cec9e32d9b3ee07fcf2a5600b6dda6d86bea
史春阳 <shichunyang02@meituan.com>
2022-06-29 13:45:49 +0800

    JobRegistryDomainService.java

---
+++ src/main/java/com/wx/domain/job/service/JobRegistryDomainService.java
@@ -0,0 +1,28 @@
+package com.wx.domain.job.service;
+
+import com.scy.core.thread.ThreadPoolUtil;
+import com.wx.dao.warehouse.mapper.JobRegistryDOMapper;
+import com.wx.domain.job.entity.JobRegistryEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author : shichunyang
+ * Date    : 2022/6/29
+ * Time    : 1:34 下午
+ * ---------------------------------------
+ * Desc    : JobRegistryDomainService
+ */
+@Service
+public class JobRegistryDomainService {
+
+    private static final ThreadPoolExecutor THREAD_POOL_EXECUTOR = ThreadPoolUtil.getThreadPool("job-registry", 1, 5, 5);
+
+    @Autowired
+    private JobRegistryDOMapper jobRegistryMapper;
+
+    public void registry(JobRegistryEntity jobRegistryEntity) {
+    }
+}
