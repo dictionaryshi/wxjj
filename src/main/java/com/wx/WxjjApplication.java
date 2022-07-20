ad168a1a64746484bb9b54795228cc268bff4c6d
史春阳 <shichunyang02@meituan.com>
2022-07-20 11:25:33 +0800

    去除无用代码

--- src/main/java/com/wx/WxjjApplication.java
+++ src/main/java/com/wx/WxjjApplication.java
@@ -1,18 +1,11 @@
 package com.wx;
 
-import com.scy.core.reflect.ReflectionsUtil;
 import com.scy.core.rest.ResponseResult;
 import com.scy.netty.rpc.consumer.RpcReference;
 import com.scy.netty.util.NettyUtil;
-import com.wx.dao.warehouse.mapper.extend.SkuStockDOMapperExtend;
-import com.wx.dao.warehouse.model.SkuStockDetailDO;
-import com.wx.dao.warehouse.model.UserPassportDO;
-import com.wx.domain.passport.entity.UserPassportEntity;
 import com.wx.netty.service.RpcModel;
 import com.wx.netty.service.UserService;
-import io.netty.util.internal.PlatformDependent;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -21,15 +14,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * WxjjApplication
@@ -44,9 +33,6 @@ import java.util.concurrent.atomic.AtomicLong;
         KafkaAutoConfiguration.class,
 })
 public class WxjjApplication {
-
-    @Autowired
-    private SkuStockDOMapperExtend skuStockDOMapperExtend;
 
     @RpcReference(version = "1.0", timeout = 6000)
     private UserService userService;
@@ -70,25 +56,5 @@ public class WxjjApplication {
         ResponseResult<RpcModel> responseResult = responseResultFuture.get(500, TimeUnit.MILLISECONDS);
         rpcModel = responseResult.getData();
         return ResponseResult.success(rpcModel);
-    }
-
-    @GetMapping("/batch")
-    public String batch() {
-        List<SkuStockDetailDO> skuStockDetails = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            SkuStockDetailDO skuStockDetailDO = new SkuStockDetailDO();
-            skuStockDetailDO.setStockBaseInfoId((long) i);
-            skuStockDetailDO.setSkuId((long) i + 1);
-            skuStockDetailDO.setType(i + 2);
-            skuStockDetailDO.setStockOffset((long) i + 3);
-            skuStockDetailDO.setStockBefore((long) i + 4);
-            skuStockDetailDO.setStockAfter((long) i + 5);
-            skuStockDetailDO.setOrderId((long) i + 6);
-            skuStockDetailDO.setOperator((long) i + 7);
-            skuStockDetailDO.setRemark((i + 8) + "");
-            skuStockDetails.add(skuStockDetailDO);
-        }
-        skuStockDOMapperExtend.batchInsert(skuStockDetails);
-        return "done";
     }
 }
--- src/main/java/com/wx/dao/warehouse/mapper/extend/SkuStockDOMapperExtend.java
+++ src/main/java/com/wx/dao/warehouse/mapper/extend/SkuStockDOMapperExtend.java
@@ -1,14 +1,7 @@
 package com.wx.dao.warehouse.mapper.extend;
 
-import com.google.common.collect.Lists;
-import com.scy.core.db.SqlUtil;
-import com.wx.dao.warehouse.model.SkuStockDetailDO;
-import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
-
-import java.util.List;
-import java.util.PrimitiveIterator;
 
 /**
  * @author : shichunyang
@@ -24,8 +17,4 @@ public interface SkuStockDOMapperExtend {
 
     @Update("update `sku_stock` set `stock` = `stock` - #{stockOffset} where `id` = #{id}")
     int reduceStock(@Param("id") long id, @Param("stockOffset") long stockOffset);
-
-
-    @InsertProvider(type = A.class, method = "batchInsert")
-    int batchInsert(List<SkuStockDetailDO> skuStockDetails);
 }
